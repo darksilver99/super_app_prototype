9e0c862cbafbb8002da402e21ae7296d5cc3487c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-26 06:54:15 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/get_data_list.dart
+++ lib/custom_code/actions/get_data_list.dart
@@ -6,6 +6,9 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 Future getDataList() async {
   // Add your function code here!
   print("getDataList");
