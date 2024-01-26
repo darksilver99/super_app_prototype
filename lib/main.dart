6a0fdcdaf285a2e62208ea8a6f6c1ccdf79f56c0
Darksilver88 <anusorn@koder3.com>
2024-01-26 10:13:52 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class NavBarPage extends StatefulWidget {
 
 /// This is the private State class that goes with NavBarPage.
 class _NavBarPageState extends State<NavBarPage> {
-  String _currentPageName = 'EventsPage';
+  String _currentPageName = 'HomePage';
   late Widget? _currentPage;
 
   @override
