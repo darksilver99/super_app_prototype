573438108ca2384287197cd790ec389837fab12c
Darksilver88 <anusorn@koder3.com>
2024-01-29 16:57:14 +0700

    a

--- lib/custom_code/actions/get_data_list.dart
+++ lib/custom_code/actions/get_data_list.dart
@@ -4,6 +4,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
 import 'package:flutter/material.dart';
+
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
@@ -12,17 +13,20 @@ import 'package:http/http.dart' as http;
 
 Future getDataList(String? str) async {
   // Add your function code here!
-  await Future.delayed(Duration(milliseconds: 1200));
+  //await Future.delayed(Duration(milliseconds: 600));
   print("getDataList");
 
+  str = str ?? "";
+  print("strstr");
+  print(str);
   Map<String, String> header = {};
   var body = {"str": str};
 
   //widget
   //var url = "https://silver-api.com/webboard/App_api_v1/testDataList";
-  var url = "http://43.254.133.98:3033/api/recommend/list/app";
-  var response =
-      await http.post(Uri.parse(url), body: jsonEncode(body), headers: header);
+  var url = "http://43.254.133.98:3033/api/recommend/list/app?str=$str";
+  //var response = await http.post(Uri.parse(url), body: jsonEncode(body), headers: header);
+  var response = await http.get(Uri.parse(url));
   if (response.statusCode == 200) {
     List<dynamic> jsonData = json.decode(response.body);
     List<TestListJsonStruct> list = jsonData
@@ -31,15 +35,16 @@ Future getDataList(String? str) async {
               subject: jsonItem['subject'],
               image: jsonItem['image'],
               createDate: jsonItem['createDate'],
+              type: jsonItem['type'],
             ))
         .toList();
     FFAppState().testList = list;
   }
 
   //event
-  var url2 = "http://43.254.133.98:3033/api/recommend/list/event";
-  var response2 =
-      await http.post(Uri.parse(url2), body: jsonEncode(body), headers: header);
+  var url2 = "http://43.254.133.98:3033/api/recommend/list/event?str=$str";
+  //var response2 = await http.post(Uri.parse(url2), body: jsonEncode(body), headers: header);
+  var response2 = await http.get(Uri.parse(url2));
   if (response2.statusCode == 200) {
     List<dynamic> jsonData = json.decode(response2.body);
     List<TestListJsonStruct> list = jsonData
@@ -48,6 +53,7 @@ Future getDataList(String? str) async {
               subject: jsonItem['subject'],
               image: jsonItem['image'],
               createDate: jsonItem['createDate'],
+              type: jsonItem['type'],
             ))
         .toList();
     FFAppState().eventList = list;
