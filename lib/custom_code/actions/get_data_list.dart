dfa2482e5fe8b34cc2faea4e345d4b56c76bb38b
Darksilver88 <anusorn@koder3.com>
2024-02-05 09:18:47 +0700

    a

--- lib/custom_code/actions/get_data_list.dart
+++ lib/custom_code/actions/get_data_list.dart
@@ -23,39 +23,53 @@ Future getDataList(String? str) async {
   var body = {"str": str};
 
   //widget
-  var url = "https://silver-api.com/webboard/App_api_v1/testDataList?type=app&str=$str";
-  //var url = "http://43.254.133.98:3033/api/recommend/list/app?str=$str";
+  var url1 = "https://silver-api.com/webboard/App_api_v1/testDataList?type=app&str=$str";
+  var url1_2 = "http://43.254.133.98:3033/api/recommend/list/app?str=$str";
   //var response = await http.post(Uri.parse(url), body: jsonEncode(body), headers: header);
-  var response = await http.get(Uri.parse(url));
+  var response;
+  try {
+    response = await http.get(Uri.parse(url1_2));
+  } catch (e) {
+    response = await http.get(Uri.parse(url1));
+  }
+  print("response");
+  print(response);
   if (response.statusCode == 200) {
     List<dynamic> jsonData = json.decode(response.body);
     List<TestListJsonStruct> list = jsonData
-        .map((jsonItem) => TestListJsonStruct(
-              id: jsonItem['id'],
-              subject: jsonItem['subject'],
-              image: jsonItem['image'],
-              createDate: jsonItem['createDate'],
-              type: jsonItem['type'],
-            ))
+        .map((jsonItem) =>
+        TestListJsonStruct(
+          id: jsonItem['id'],
+          subject: jsonItem['subject'],
+          image: jsonItem['image'],
+          createDate: jsonItem['createDate'],
+          type: jsonItem['type'],
+        ))
         .toList();
     FFAppState().testList = list;
   }
 
   //event
   var url2 = "https://silver-api.com/webboard/App_api_v1/testDataList?type=event&str=$str";
-  //var url2 = "http://43.254.133.98:3033/api/recommend/list/event?str=$str";
+  var url2_2 = "http://43.254.133.98:3033/api/recommend/list/event?str=$str";
   //var response2 = await http.post(Uri.parse(url2), body: jsonEncode(body), headers: header);
-  var response2 = await http.get(Uri.parse(url2));
+  var response2;
+  try {
+    response2 = await http.get(Uri.parse(url2_2));
+  } catch (e) {
+    response2 = await http.get(Uri.parse(url2));
+  }
   if (response2.statusCode == 200) {
     List<dynamic> jsonData = json.decode(response2.body);
     List<TestListJsonStruct> list = jsonData
-        .map((jsonItem) => TestListJsonStruct(
-              id: jsonItem['id'],
-              subject: jsonItem['subject'],
-              image: jsonItem['image'],
-              createDate: jsonItem['createDate'],
-              type: jsonItem['type'],
-            ))
+        .map((jsonItem) =>
+        TestListJsonStruct(
+          id: jsonItem['id'],
+          subject: jsonItem['subject'],
+          image: jsonItem['image'],
+          createDate: jsonItem['createDate'],
+          type: jsonItem['type'],
+        ))
         .toList();
     FFAppState().eventList = list;
   }
