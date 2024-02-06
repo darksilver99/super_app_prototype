74b5ed20b1f01daeb815e7b8e1b28b001d15004e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-06 06:56:06 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/set_recombee_data.dart
+++ lib/custom_code/actions/set_recombee_data.dart
@@ -9,4 +9,11 @@ import 'package:flutter/material.dart';
 
 Future setRecombeeData(String? id) async {
   // Add your function code here!
+  try {
+    print("setRecombeeData $id");
+    await http
+        .get(Uri.parse("http://43.254.133.98:3033/api/recommend/buy/$id"));
+  } catch (e) {
+    print(e);
+  }
 }
